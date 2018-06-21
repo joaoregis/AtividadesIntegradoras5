9f240babbfa3e42fc0c942b47439425ca03444b0
João Regis <joao.reegiis@gmail.com>
2018-06-21 19:40:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import sys
-from ConnetionUDP import ConnetionUDP
 from LDR import LDR
 from LED import LED
 from USC import UniversalCoverter
@@ -7,7 +6,7 @@ from OrderScale import OrderScale
 import socket
 
 
-address = "192.168.43.107"
+address = "192.168.43.161"
 
 myled = LED()
 
@@ -17,7 +16,6 @@ rbscale = UniversalCoverter(100,10,OrderScale.DESCENDING)
 
 nmcuscale = UniversalCoverter(0,100, OrderScale.ASCENDING)
 
-
 sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM)
 sock.bind((address, 555))
 
@@ -25,29 +23,23 @@ while True:
     
     print('Waiting to receive anything ...')
     data, addr = sock.recvfrom(1024)
-    
-    #data, addr = conn.ReciveData()
 
     if data.decode() != '':
-        print ('Raw data ::: ', data)
+        print ('NodeMCU Value ::: ', data)
         nodeldrvalue = nmcuscale.GetValueUniversalScale(float(data.decode()))
-        print ('NodeMCU Value ::: ',  nodeldrvalue, '\n')
 
     raspberryldrvalue = nmcuscale.GetValueUniversalScale(myldr.GetLDRCount())
     
     print ('Rasperry Value ::: ',  raspberryldrvalue, '\n')
     
-    if (abs( nodeldrvalue - raspberryldrvalue ) < 4):
+    if (abs( nodeldrvalue - raspberryldrvalue ) < 8):
         sock.sendto('2'.encode(), addr)
-        #conn.SendData(2, addr)
         myled.sendsignalled("blink")
         
     elif (nodeldrvalue > raspberryldrvalue):
         sock.sendto('0'.encode(), addr)
-        #conn.SendData(0, addr)
         myled.sendsignalled("off")
 
     elif (nodeldrvalue < raspberryldrvalue):
         sock.sendto('1'.encode(), addr)
-        #conn.SendData(1, addr)
         myled.sendsignalled("on")
